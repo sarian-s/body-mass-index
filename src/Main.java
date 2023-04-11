e60d56ac6f47324499a2d701880a2769ad303f85
sergey s <som-ana@yandex.ru>
2023-04-11 03:35:45 +0300

    added calculations commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
+        /*
+        System.out.println(service.calculate(45, 90));
+        */
+
         System.out.printf("%.1f", service.calculate(45, 90));
+        System.out.println();
+
+        System.out.printf("%.1f", service.calculate(100, 100));
+        System.out.println();
 
         System.out.println();
         System.out.print("Мой ИМТ ");
