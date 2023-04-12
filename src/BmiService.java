1c4ce7b60848e487c378b6ba94043df2d98d23bc
sergey s <som-ana@yandex.ru>
2023-04-12 22:32:04 +0300

    int index

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,9 @@
 public class BmiService {
-    public double calculate(int weight, double height){
-        double heightToM = height/100;
-        double result = weight/(heightToM*heightToM);//weight-масса ,height-рост
-        return result;//= Math.round(result*10.0)/10.0;
-
+    public int calculate(int weight, double height) {
+        double heightToM = height / 100;
+        double result = weight / (heightToM * heightToM);//weight-масса ,height-рост
+        int bmi = (int) result;
+        return bmi;
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         BmiService service = new BmiService();
 
         /*
         System.out.println(service.calculate(45, 90));
         */
 
-        System.out.printf("%.1f", service.calculate(45, 90));
-        System.out.println();
+        System.out.println(service.calculate(45, 90));
 
-        System.out.printf("%.1f", service.calculate(100, 100));
-        System.out.println();
+        System.out.println(service.calculate(100, 100));
 
-        System.out.println();
         System.out.print("Мой ИМТ ");
-        System.out.printf("%.1f", service.calculate(87, 182));
-        System.out.println(" кг/м2");
+        System.out.print(service.calculate(87, 182));
+        System.out.print(" кг/м2");
 
     }
-
 }
